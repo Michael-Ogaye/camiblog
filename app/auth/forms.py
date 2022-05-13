40814144abaa4e283d37b187eb55287fe79e2efb
Michael Ogaye <hogaeymic@gmail.com>
2022-05-13 21:29:51 +0300

    ft; signup form rendering

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,BooleanField
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,ValidationError
 from wtforms.validators import DataRequired,Email,EqualTo
+from ..models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -9,6 +10,15 @@ class RegistrationForm(FlaskForm):
     password1 = PasswordField('Password', validators = [DataRequired()])
     password2 = PasswordField('Confirm Password', validators = [DataRequired(),EqualTo('password1')])
     submit = SubmitField('Register')
+
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError(message="The Email has already been taken!")
+    
+    def validate_username(self, data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError(message="The username has already been taken")
 
 class LoginForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(), Email()])
--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,7 @@ from . import auth
 from .forms import RegistrationForm,LoginForm
 from flask_login import login_user,logout_user,login_required,current_user
 from ..models import User
-from flask import redirect,render_template,url_for,flash
+from flask import redirect,render_template,url_for,flash,request
 from .. import db
 
 
@@ -19,5 +19,19 @@ def register():
         user.set_password(form.password1.data)
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for('login'))
-    return render_template('registration.html', form=form)
+        return redirect(url_for('auth.login'))
+    return render_template('auth/signup.html', form=form)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email = form.email.data).first()
+        if user is not None and user.check_password(form.password.data):
+            login_user(user)
+            next = request.args.get("next")
+            return redirect(next or url_for('main.index'))
+        flash('Invalid email address or Password.')    
+    return render_template('auth/login.html', form=form)
